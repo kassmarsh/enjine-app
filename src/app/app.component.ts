c22a739ead0586ff3a08d7e6be3ed4181ff2fada
Kassidy Marsh <kmarsh08@mail.uoguelph.ca>
2019-03-03 12:49:14 -0500

    Added button onclick event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,11 @@ import bsCustomFileInput from 'bs-custom-file-input';
 export class AppComponent {
   title = 'ETF Comparison Web Application';
   
-  ngOnInit() { bsCustomFileInput.init(); }
+  ngOnInit() { 
+  	bsCustomFileInput.init();
+  	document.getElementById("compare").onclick = function () {
+      var file1 = document.getElementById("file1");
+      var file2 = document.getElementById("file2");
+    };
+  }
 }
