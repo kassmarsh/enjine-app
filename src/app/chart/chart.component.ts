934f312e091fe05f51c3980bee490cf2e5d99bec
Kassidy Marsh <kmarsh08@mail.uoguelph.ca>
2019-03-04 14:15:04 -0500

    store csvs locally as strings

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chart',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart.component.css']
 })
 export class ChartComponent implements OnInit {
+  
+  @Input('firstFile') file1: string;
+  @Input('secondFile') file2: string;
 
   constructor() { }
 
   ngOnInit() {
+    document.getElementById("test1").innerHTML = localStorage.getItem('file1');
+    document.getElementById("test2").innerHTML = localStorage.getItem('file2');
   }
 
 }
