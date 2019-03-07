003b0d362497a9f145a6a5ea3e78aead835c17c9
Kassidy Marsh <kmarsh08@mail.uoguelph.ca>
2019-03-07 13:17:47 -0500

    fixed code to handle duplicates in ETF files

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -18,6 +18,9 @@ export class ChartComponent implements OnInit {
         document.getElementById("chartContainer").classList.add("d-none");
         return;
     }
+    //get rid of trailing newlines (had to do this after checking nullness)
+    firstFile = firstFile.trim();
+    secondFile = secondFile.trim();
 
     //var fileArray1 = firstFile.match(/(".*?"|[^",]+)(?=\s*,|\s*$)/g);
     //var fileArray2 = secondFile.match(/(".*?"|[^",]+)(?=\s*,|\s*$)/g);
@@ -31,8 +34,6 @@ export class ChartComponent implements OnInit {
     	var smallerFile = firstFile.split("\n");
     }
 
-    //var fileArray1 = firstFile.split("\n");
-    //var fileArray2 = secondFile.split("\n");
     var title1 = biggerFile[0].match(/[^,]*/);
     var title2 = smallerFile[0].match(/[^,]*/);
 
@@ -40,36 +41,66 @@ export class ChartComponent implements OnInit {
     var biggerValues = [];
     var smallerNames = [];
     var smallerValues = [];
-    var labels = [];
+    var duplicateNames = [];
+    var duplicateCounts = [];
     var temp;
     var temp2;
+    var temp3;
+    var temp4;
     var i;
+    var j = 0;
 
     //line 6 is where data values begin
     for (i = 6; i < smallerFile.length; i++) {
     	temp = smallerFile[i].split(",");
-    	smallerNames[i-6] = temp[0];
-    	smallerValues[i-6] = temp[1].match(/[^%]*/);
-    	labels[i-6] = i-6;
+    
+        temp2 = smallerNames.indexOf(temp[0]);
+        //duplicate - sum % of market value
+        if (temp2 != -1) {
+            temp3 = parseFloat(smallerValues[temp2]);
+            temp3 += parseFloat(temp[1].replace(/[=%"]/g, ""));
+            smallerValues[temp2] = temp3.toString();
+            //do not advance j index
+        }
+        else {
+    	   smallerNames[j] = temp[0];
+    	   smallerValues[j] = temp[1].replace(/[=%"]/g, "");
+           j++;
+        }
     }
+
+    j = 0;
     for (i = 6; i < biggerFile.length; i++) {
     	temp = biggerFile[i].split(",");
-    	biggerNames[i-6] = temp[0];
-    	biggerValues[i-6] = temp[1].match(/[^%]*/);
-    	temp2 = smallerNames.indexOf(biggerNames[i-6]);
-    	if (temp2 == -1) {
-    		//smaller file doesn't have holding name
-    		smallerNames.splice((i-6), 0, biggerNames[i-6]);
-    		smallerValues.splice((i-6), 0, "0");
-    	}
-    	//smaller file has holding name but it's in wrong spot
-    	else if (temp2 != (i-6)) {
-    		smallerNames.splice(temp2, 1);
-    		smallerValues.splice(temp2, 1);
-    		smallerNames.splice((i-6), 0, biggerNames[i-6]);
-    		smallerValues.splice((i-6), 0, "0");
-    	}
+        
+        temp3 = biggerNames.indexOf(temp[0]);
+        //duplicate detected
+        if (temp3 != -1) {
+            temp4 = parseFloat(biggerValues[temp3]);
+            temp4 += parseFloat(temp[1].replace(/[=%"]/g, ""));
+            biggerValues[temp3] = temp4.toString();
+        }
+        else {
+    	   biggerNames[j] = temp[0];
+    	   biggerValues[j] = temp[1].replace(/[=%"]/g, "");
+
+            temp2 = smallerNames.indexOf(biggerNames[j]);
+        	if (temp2 == -1) {
+        		//smaller file doesn't have holding name
+        		smallerNames.splice((j), 0, biggerNames[j]);
+        		smallerValues.splice((j), 0, "0");
+        	}
+        	//smaller file has holding name but it's in wrong spot
+        	else if (temp2 != (j)) {
+        		smallerNames.splice(temp2, 1);
+        		smallerValues.splice(temp2, 1);
+        		smallerNames.splice((j), 0, biggerNames[j]);
+        		smallerValues.splice((j), 0, "0");
+        	}
+            j++;
+        }
     }
+
     //there were some holding names in the smaller file not in the larger
     if (biggerNames.length < smallerNames.length) {
     	for (i = 0; i < smallerNames.length; i++) {
@@ -98,6 +129,11 @@ export class ChartComponent implements OnInit {
 			}]
 		},
 		options: {
+            layout: {
+                padding: {
+                    right: 10
+                }
+            },
 			responsive: true,
 			title: {
 				display: true,
